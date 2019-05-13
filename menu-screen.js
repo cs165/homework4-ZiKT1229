dc4d37614bff005975ea11aa9d42988a488fefd0
KT_Zheng <a6163508@gmail.com>
2019-05-13 20:29:31 +0800

    song-selector 暫時完成

--- menu-screen.js
+++ menu-screen.js
@@ -5,6 +5,42 @@
 class MenuScreen {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
+    this.songSelector = document.getElementById('song-selector');
+    this.fetchSong();
   }
   // TODO(you): Add methods as necessary.
+  async fetchSong() {
+    const url = 'https://cs165.github.io/homework4-ZiKT1229/playlist/songs.json';
+
+    try {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        this.render(jsonResponse);
+      } else {
+        throw new Error('Error!!!!!!!!!!!!!!!!!!!!!!');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render(songs) {
+    Object.keys(songs).forEach((songName) => {
+      console.log(songs[songName]);
+      const data = songs[songName];
+      const option = this.$create('option', { 'data-url': `${data.songUrl}` }, `${data.artist} - ${data.title}`);
+      this.songSelector.appendChild(option);
+    });
+  }
+
+  $create(tag = 'div', attrs = {}, text = '') {
+    const node = document.createElement(tag);
+    Object.keys(attrs).forEach((name) => {
+      node.setAttribute(name, attrs[name]);
+    });
+    node.textContent = text;
+    return node;
+  };
 }
